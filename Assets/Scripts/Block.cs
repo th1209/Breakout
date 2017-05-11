e1f58bd13a4caffe75b76554ab263341b8cabc68
Toshiki Hata <t.hata@oratta.net>
2017-05-11 23:32:49 +0900

    ステージやブロックにエフェクトを付けたり、ブロック破壊時にエフェクトを付与

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,44 +4,61 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    // このブロックが壊れるための、ヒット回数。
-    public int breakCount = 1;
+    // このブロックの表面を決める、マテリアル配列。
+    public Material[] materialPrefabs;
 
-    // このブロックにヒットした際に得られるスコア。
-    public int hitScore = 100;
+    // このブロックが壊れるための、ヒット回数。
+    protected int breakCount;
 
     // このブロックが、ボールとヒットした回数。breakCountに達した時点でDestroy。
     protected int hitCount;
 
+    // このブロックにヒットした際に得られるスコア。
+    protected int hitScore;
+
     // 通知先コントローラ。
     protected MainController controller;
 
-    public void Start ()
+    // 破壊用オブジェクト。
+    protected BlockDestroyer destroyer;
+
+
+    // ヒットした際に得られるスコアのデフォルト値。
+    protected static int defaultHitScore = 100;
+
+    public AudioClip breakSe;
+
+    public GameObject explosionPrefab;
+
+    /// <summary>
+    /// ブロックに関する諸々の初期化処理を行う。ブロック初期化時に、必ずこのメソッドを呼び出すこと。
+    /// </summary>
+    /// <param name="breakCount">何回ヒットでこのブロックが壊れるか。</param>
+    public void Initialze (int breakCount)
     {
-        controller = GameObject.Find("MainController").GetComponent<MainController>();
-        hitCount = 0;
+        this.controller = GameObject.Find("MainController").GetComponent<MainController>();
+        this.destroyer = GameObject.Find("BlockDestroyer").GetComponent<BlockDestroyer>();
+        this.hitCount = 0;
+        // TODO ヒット時スコアは決め打ち。ブロックの硬さに応じてヒット時スコアが変わるようにするのも良いかも。
+        this.hitScore = Block.defaultHitScore;
+
+        this.breakCount = breakCount;
+        this.GetComponent<Renderer>().material= materialPrefabs[breakCount - 1];
     }
 
-    public void SetBreakCount (int breakCount)
-    {
-        this.breakCount = breakCount;
-    }
 
     void OnCollisionEnter(Collision other)
     {
         // ボールとぶつかった際の処理。
         if (other.gameObject.tag == "Ball") {
-            hitCount++;
+            this.hitCount++;
+            controller.AddScore(this.hitScore);
 
             // 破壊時処理。
-            if (hitCount >= breakCount) {
-                // TODO: 効果音
-                // TODO: 破壊時Effect
-                Destroy(this.gameObject);
+            if (this.hitCount >= this.breakCount)
+            {
+                destroyer.DestroyBlock(this);
             }
-
-            // スコア加算。
-            controller.AddScore(hitScore);
         }
     }
 }
---
+++ Assets/Scripts/BlockDestroyer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// ブロック破壊時の諸々の処理を行うクラス。
+/// </summary>
+/// <remarks>
+/// 本来は「ブロック破壊」という機能でなく、
+/// 「SE再生」や「エフェクト再生」という実装で、クラスを作るべき(その方が汎用性が高いから)。
+/// e.g.) AudioPlayer, EffectGenerator
+/// 今回は時間がないのとC#力が無いことを言い訳にして、このようなやっつけクラスを作っているが、今後修正を入れたほうが良いはず...。
+/// </remarks>
+public class BlockDestroyer : MonoBehaviour
+{
+    // ブロック破壊時効果音。
+    public AudioClip breakSe;
+
+    // ブロック破壊時エフェクト。
+    public GameObject explosionEffectPrefab;
+
+    /// <summary>
+    /// ブロック破壊時の処理をまとめて実施する。
+    /// </summary>
+    /// <remarks>SEもエフェクトも、同時に一定数以上の数が再生されないように調整すべきかも。</remarks>
+    /// <param name="block">Block.</param>
+    public void DestroyBlock(Block block)
+    {
+        this.gameObject.GetComponent<AudioSource>().PlayOneShot(breakSe);
+
+        GameObject effect = (GameObject)Instantiate(explosionEffectPrefab, block.gameObject.transform.position, Quaternion.identity);
+        // エフェクト終了までに時間がかかるので、一定時間待ってからDestroy。
+        Destroy(effect, 3f);
+
+        Destroy(block.gameObject);
+    }
+}
